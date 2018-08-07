d2de2d259211850f7c597c233b37b99f883c8a62
v1sanjay <39975088+v1sanjay@users.noreply.github.com>
2018-08-07 11:15:59 -0400

    Add files via upload

---
+++ ID.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.reductables.mainPackage;
+
+/**
+ * @author tvsan
+ *
+ */
+public enum ID {
+	Player{},
+	TrashEnemy{},
+	Trail{},
+	TrailEnemy{},
+	FollowEnemy{},
+	FallEnemy{},
+	BossEnemy1{},
+	TextQuestion{}
+}
