fdce11aa70822dbf9a6bca16a2ee5e8e61ea411a
v1sanjay <39975088+v1sanjay@users.noreply.github.com>
2018-08-07 11:15:48 -0400

    Add files via upload

---
+++ Handler.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.reductables.mainPackage;
+import java.awt.Graphics;
+import java.util.LinkedList;
+/**
+ * @author tvsan
+ *
+ */
+public class Handler {
+	
+	LinkedList<GameObject> object = new LinkedList<GameObject>();
+	
+	public void tick() {
+		for (int i = 0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			tempObject.tick();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for (int i = 0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			tempObject.render(g);
+		}
+	}
+	
+	public void addObject(GameObject object) {
+		this.object.add(object);
+	}
+	
+	public void removeObject(GameObject object) {
+		this.object.remove(object);
+	}
+}
